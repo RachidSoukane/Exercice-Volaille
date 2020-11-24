bf40dfce5e8ad905d25b4a7bb4ed0276ee49f386
afguel <afguel5927@gmail.com>
2020-11-24 18:54:12 +0100

    finalisation de la class Canard et Poulet

--- src/com/coaching/volaille/volailleAVendre/Canard.java
+++ src/com/coaching/volaille/volailleAVendre/Canard.java
@@ -2,6 +2,49 @@ package com.coaching.volaille.volailleAVendre;
 
 public final class Canard extends VolailleAVendre {
 
+	public static final int nombreCanardMax = 4;
+	private static int nombreCanard;
+	private static int poidAbatage=2200;
+	private static int prixKilo;
+	
+
+	public  Canard(int poids) {
+		super(poids);
+	}
+	
+	public static int getPoidAbatage() {
+		return poidAbatage;
+	}
+
+
+	public static void setPoidAbatage(int poidAbatage) {
+		Canard.poidAbatage = poidAbatage;
+	}
+
+
+	public static int getPrixKilo() {
+		return prixKilo;
+	}
+
+
+	public static void setPrixKilo(int prixKilo) {
+		Canard.prixKilo = prixKilo;
+	}
+
+	public int getNombre() {
+		return nombreCanard;
+	}
+
+	public void setNombre(int nombreCanard) {
+		this.nombreCanard = nombreCanard;
+	}
+
+	@Override
+	public String affiche() {
+		return"canard";
+		
+	}
+	
 	
 
 	
--- src/com/coaching/volaille/volailleAVendre/Poulet.java
+++ src/com/coaching/volaille/volailleAVendre/Poulet.java
@@ -2,6 +2,49 @@ package com.coaching.volaille.volailleAVendre;
 
 public final class Poulet extends VolailleAVendre {
 
+	public static final int nombrePouletMax = 5;
+	private static int nombrePoulet;
+	private static int poidAbatage=800;
+	static int prixKilo;
+
+	public Poulet(int poids) {
+		super(poids);
+	}
+	
+	public static int getPoidAbatage() {
+		return poidAbatage;
+	}
+
+
+	public static void setPoidAbatage(int poidAbatage) {
+		Poulet.poidAbatage = poidAbatage;
+	}
+
+
+	public static int getPrixKilo() {
+		return prixKilo;
+	}
+
+
+	public static void setPrixKilo(int prixKilo) {
+		Poulet.prixKilo = prixKilo;
+	}
+
+
+	public int getNombre() {
+		return nombrePoulet;
+	}
+
+	public void setNombre(int nombreCanard) {
+		this.nombrePoulet = nombreCanard;
+	}
+
+
+	@Override
+	public String affiche() {
+		return "poulet";
+		
+	}
 	
 	
 
